a5477f195b88f2eccd1b6c0967eae2be0a4c8379
raylandho123 <47544928+raylandho123@users.noreply.github.com>
2021-08-26 20:42:50 -0700

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by hfu on 8/21/21.
+//  Copyright © 2021 hfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
